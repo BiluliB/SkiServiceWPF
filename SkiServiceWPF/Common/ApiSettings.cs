38a6561855490a4c08e57ddb347f4f4b8923c6a9
Bobby <138381572+BiluliB@users.noreply.github.com>
2023-12-21 20:00:02 +0100

    Connect login to backend

---
+++ SkiServiceWPF/Common/ApiSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkiServiceWPF.Common
+{
+    public class ApiSettings
+    {
+        public string BaseUrl { get; set; }
+        public string LoginEndpoint { get; set; }
+        public string UserCreationEndpoint { get; set; }
+        public string ServiceRequestsEndpoint { get; set; }
+        // Weitere Eigenschaften nach Bedarf
+    }
+}
---
+++ SkiServiceWPFBiluliBBackend/Models/LoginResponseModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkiServiceWPFBiluliBBackend.Models
+{
+    public class LoginResponseModel
+    {
+        public string Token { get; set; }
+        // Du kannst weitere Felder hinzufügen, wie z.B. Benutzerinformationen, wenn diese vom Server zurückgegeben werden
+    }
+}
