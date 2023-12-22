48e0570b7588f90275c532543421ddf922a46d17
Bobby <138381572+BiluliB@users.noreply.github.com>
2023-12-23 00:53:44 +0100

    create dtos for login

---
+++ SkiServiceWPF/DTOs/AuthRequestDto.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace SkiServiceWPF.DTOs
+{
+    /// <summary>
+    /// Data Transfer Object for authentication request
+    /// </summary>
+    public class AuthRequestDto
+    {
+        [JsonProperty("username")]
+        public string UserName { get; set; }
+
+        [JsonProperty("password")]
+        public string Password { get; set; }
+    }
+}
---
+++ SkiServiceWPF/DTOs/LoginResponseDto.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace SkiServiceWPF.DTOs
+{
+    /// <summary>
+    /// Data Transfer Object for login response
+    /// </summary>
+    public class LoginResponseDto
+    {
+        [JsonProperty("token")]
+        public string Token { get; set; }
+    }
+}
